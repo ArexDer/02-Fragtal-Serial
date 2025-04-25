26aedcff6a2d8687b40caae503a3671d874305a8
Tu Nombre <xdcristian619@gmail.com>
2025-04-25 09:25:34 -0500

    cambio 1 en la UCE

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/UNIVERSIDAD/DECIMO/PROGRA_PARALELA/Ejercicio2/02-Fragtal-Serial
+# Install script for directory: C:/Users/fing.labcom/Documents/PRACTICAS/Programacion_Paralela/Clases_Paralela/Nueva carpeta/02-Fragtal-Serial
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/UNIVERSIDAD/DECIMO/PROGRA_PARALELA/Ejercicio2/02-Fragtal-Serial/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/fing.labcom/Documents/PRACTICAS/Programacion_Paralela/Clases_Paralela/Nueva carpeta/02-Fragtal-Serial/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/UNIVERSIDAD/DECIMO/PROGRA_PARALELA/Ejercicio2/02-Fragtal-Serial/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/fing.labcom/Documents/PRACTICAS/Programacion_Paralela/Clases_Paralela/Nueva carpeta/02-Fragtal-Serial/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
